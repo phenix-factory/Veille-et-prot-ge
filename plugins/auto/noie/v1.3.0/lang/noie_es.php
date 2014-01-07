c4e496eb8484432466cf5105f816b17b0a43e6a9
phenix-factory <trash@henix.be>
2014-01-07 21:45:21 +0100

    Mise à jour des plugin, synchro avec la version.

---
+++ plugins/auto/noie/v1.3.0/lang/noie_es.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/noie?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noie_info' => 'Para navegar de manera satisfactoria en este sitio web, le recomendamos utilizar un navegador <strong>conforme a los estándares internacionales</strong>, que este vela en adoptar. Le sugerimos probar otros navegadores populares, como <a href="http://www.mozilla-europe.org/es/firefox/">Firefox</a>, <a href="http://www.google.es/chrome">Chrome</a>, <a href="http://www.opera.com/">Opera</a> o <a href="http://www.apple.com/rd/safari/">Safari</a>, o al menos pasar su <a href="http://www.microsoft.com/latam/windows/internet-explorer/">Internet Explorer</a>a versiones menos incompatibles. ', # MODIF
+	'noie_titre' => '¡OjO: Su Internet Explorer es incompatible con las normas internacionales!',
+
+	// T
+	'toocool_alt' => 'Demasiado Cool para IE',
+	'toocool_info' => 'El sitio que estás tratando de ver ha sido juzgado "Demasiado Cool Para Internet Explorer".
+<br />Por favor tómate el tiempo de probar uno de éstos geniales navegadores : <a href="http://www.w3junkies.com/toocool/index.php?language=es">http://www.w3junkies.com/toocool/</a>',
+	'toocool_titre' => 'Demasiado Cool para Internet Explorer'
+);
+
+?>
---
+++ plugins/auto/noie/v1.3.0/lang/noie_it.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/noie?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// N
+	'noie_info' => 'Per navigare meglio su questo sito su tutto il Web, ti consigliamo di <strong>aggiornare il tuo <a href="http://www.microsoft.com/italy/windows/internet-explorer/">Internet Explorer</a></string> o di provare un altro browser popolare come <a href="http://www.mozilla-europe.org/it/firefox/">Firefox</a>, <a href="http://www.google.it/chrome/">Chrome</a>, <a href="http://www.opera.com/browser/">Opera</a> o <a href="http://www.apple.com/it/safari/">Safari</a>.', # MODIF
+	'noie_titre' => 'Attenzione, il tuo Internet Explorer non è aggiornato!',
+
+	// T
+	'toocool_alt' => 'Too Cool for IE',
+	'toocool_info' => 'Il sito che stai tentando di vedere è certificato "Too Cool for Internet Explorer".
+<br />Per favore, prendi un momento di tempo e prova uno di questi grandi browsers: <a href="http://www.w3junkies.com/toocool/index.php?language=it">http://www.w3junkies.com/toocool/</a>',
+	'toocool_titre' => 'Too Cool for Internet Explorer'
+);
+
+?>
